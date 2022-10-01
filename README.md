af64b0e5b52d6fecb534c4a56622cbad7e25d78f
Utkarsh Srivastava <usrivastava92@gmail.com>
2022-10-01 10:22:06 +0530

    Added Unit Tests (#3)
    * added unit tests
    
    Co-authored-by: Utkarsh Srivastava <usrivastava@atlassian.com>

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Insomnia Plugin - Atlassian ASAP/SLAUTH Token Generator
 
-![GitHub Workflow Status (event)](https://img.shields.io/github/workflow/status/usrivastava92/insomnia-plugin-atlassian-asap-slauth/Node.js%20CI)
+![Build Status](https://img.shields.io/github/workflow/status/usrivastava92/insomnia-plugin-atlassian-asap-slauth/Node.js%20CI)
+![License](https://img.shields.io/github/license/usrivastava92/insomnia-plugin-atlassian-asap-slauth)
+[![](https://img.shields.io/npm/v/insomnia-plugin-atlassian-asap-slauth.svg)](https://www.npmjs.com/package/insomnia-plugin-atlassian-asap-slauth)
+[![](https://img.shields.io/badge/insomnia-install%20plugin-purple.svg?color=6a57d5)](https://insomnia.rest/plugins/insomnia-plugin-atlassian-asap-slauth)
+![Downloads](https://img.shields.io/npm/dm/insomnia-plugin-atlassian-asap-slauth)
 
 This is a plugin for [Insomnia](https://insomnia.rest) that enables you to generate Atlassian SLAUTH or ASAP tokens.
 Commands are executed using `child_process`.`exec`. ie., equivalent to `/bin/sh` `<cmd-specified>`
@@ -10,10 +14,10 @@ Commands are executed using `child_process`.`exec`. ie., equivalent to `/bin/sh`
 Go to _Insomnia > Preferences > Plugins_, type in `insomnia-plugin-atlassian-asap-slauth` and click Install Plugin.
 
 ## Usage
-Insert function either in headers or body either by typing `ctrl + space` or start type `{{`
+Insert function either in headers or body either by typing `ctrl + space` or start typing `{{asap/`
 
 - Choose your token type
 - Fill mandatory or conditional mandatory fields depending on your token type
 
 See screenshot below
-![Screenshot](https://github.com/usrivastava92/insomnia-plugin-atlassian-asap-slauth/blob/master/example.png)
+![Screenshot](https://github.com/usrivastava92/insomnia-plugin-atlassian-asap-slauth/blob/master/example.png?raw=true)
---
+++ index.test.js
@@ -0,0 +1,49 @@
+const {run} = require("./index.js");
+
+describe("Test run", () => {
+  test("Test run with slauth token type when envType not provided", async () => {
+    const audience = "micros-service";
+    const envType = "";
+    const slauthGroup = "grp";
+    const tokenType = "slauth";
+    const output = await run("", tokenType, audience, envType, slauthGroup, "");
+    expect(output).toBe("invalid value defined for `envType`");
+  });
+
+  test("Test run with slauth token type when invalid envType is provided", async () => {
+    const audience = "micros-service";
+    const envType = "invalid";
+    const slauthGroup = "grp";
+    const tokenType = "slauth";
+    const output = await run("", tokenType, audience, envType, slauthGroup, "");
+    expect(output).toBe("invalid value defined for `envType`");
+  });
+
+  test("Test run with slauth token type when audience not provided", async () => {
+    const audience = "";
+    const envType = "staging";
+    const slauthGroup = "grp";
+    const tokenType = "slauth";
+    const output = await run("", tokenType, audience, envType, slauthGroup, "");
+    expect(output).toBe("invalid value defined for `audience`");
+  });
+
+  test("Test run with asap token type when asapConfigFilePath is not provided", async () => {
+    const audience = "aud";
+    const tokenType = "asap";
+    const output = await run("", tokenType, audience, "", "", "");
+    expect(output).toBe("invalid value defined for `asapConfigFilePath`");
+  });
+
+  test("Test run with asap token type when audience is not provided", async () => {
+    const asapConfigFilePath = "grp";
+    const tokenType = "asap";
+    const output = await run("", tokenType, "", "", "", asapConfigFilePath);
+    expect(output).toBe("invalid value defined for `audience`");
+  });
+
+  test("Test run with invalid token type", async () => {
+    const output = await run("", "invalid", "", "", "", "");
+    expect(output).toBe("unknown token type : invalid");
+  });
+});
