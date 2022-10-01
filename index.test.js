24d694cc62bf0d20f88dc8d5a9ad5f00b4bcd534
Utkarsh Srivastava <usrivastava@atlassian.com>
2022-10-01 10:16:21 +0530

    added unit test

--- index.test.js
+++ index.test.js
@@ -1,23 +1,6 @@
 const { run } = require("./index.js");
 
-jest.mock("./index.js", () => ({
-  ...jest.requireActual("./index.js"),
-  executeCommandSafely: jest.fn().mockResolvedValue("foo")
-}));
-
 describe("Test run", () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
-  test("Test run with slauth token type", async () => {
-    const audience = "micros-service";
-    const envType = "staging";
-    const slauthGroup = "grp";
-    const tokenType = "slauth";
-    const output = await run("", tokenType, audience, envType, slauthGroup, "");
-    expect(output).toBe("invalid value defined for `asapConfigFilePath`");
-  });
 
   test("Test run with slauth token type when envType not provided", async () => {
     const audience = "micros-service";
@@ -26,15 +9,6 @@ describe("Test run", () => {
     const tokenType = "slauth";
     const output = await run("", tokenType, audience, envType, slauthGroup, "");
     expect(output).toBe("invalid value defined for `envType`");
-  });
-
-  test("Test run with slauth token type when slauthGroup not provided", async () => {
-    const audience = "micros-service";
-    const envType = "staging";
-    const slauthGroup = "";
-    const tokenType = "slauth";
-    const output = await run("", tokenType, audience, envType, slauthGroup, "");
-    expect(output).toBe("invalid value defined for `slauthGroup`");
   });
 
   test("Test run with slauth token type when invalid envType is provided", async () => {
@@ -53,21 +27,6 @@ describe("Test run", () => {
     const tokenType = "slauth";
     const output = await run("", tokenType, audience, envType, slauthGroup, "");
     expect(output).toBe("invalid value defined for `audience`");
-  });
-
-  test("Test run with asap token type", async () => {
-    const audience = "micros-service";
-    const asapConfigFilePath = "grp";
-    const tokenType = "asap";
-    const output = await run(
-      "",
-      tokenType,
-      audience,
-      "",
-      "",
-      asapConfigFilePath
-    );
-    expect(output).toBe("invalid value defined for `asapConfigFilePath`");
   });
 
   test("Test run with asap token type when asapConfigFilePath is not provided", async () => {
