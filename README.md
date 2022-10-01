5cd1fe07c3f374229ecb1a1ef731f4c841c1242c
Utkarsh Srivastava <usrivastava@atlassian.com>
2022-10-01 07:40:00 +0530

    added unit tests

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Insomnia Plugin - Atlassian ASAP/SLAUTH Token Generator
 
-![GitHub Workflow Status (event)](https://img.shields.io/github/workflow/status/usrivastava92/insomnia-plugin-atlassian-asap-slauth/Node.js%20CI)
+![Build Status](https://img.shields.io/github/workflow/status/usrivastava92/insomnia-plugin-atlassian-asap-slauth/Node.js%20CI)   ![License](https://img.shields.io/github/license/usrivastava92/insomnia-plugin-atlassian-asap-slauth)    ![Downloads](https://img.shields.io/npm/dm/insomnia-plugin-atlassian-asap-slauth)
 
 This is a plugin for [Insomnia](https://insomnia.rest) that enables you to generate Atlassian SLAUTH or ASAP tokens.
 Commands are executed using `child_process`.`exec`. ie., equivalent to `/bin/sh` `<cmd-specified>`
@@ -10,7 +10,7 @@ Commands are executed using `child_process`.`exec`. ie., equivalent to `/bin/sh`
 Go to _Insomnia > Preferences > Plugins_, type in `insomnia-plugin-atlassian-asap-slauth` and click Install Plugin.
 
 ## Usage
-Insert function either in headers or body either by typing `ctrl + space` or start type `{{`
+Insert function either in headers or body either by typing `ctrl + space` or start type `{{asap/`
 
 - Choose your token type
 - Fill mandatory or conditional mandatory fields depending on your token type
---
+++ index.test.js
@@ -0,0 +1,77 @@
+const { run, executeCommandSafely } = require("./index.js");
+
+jest.mock("./index.js", () => ({
+  ...jest.requireActual("./index.js"),
+  executeCommandSafely: jest.fn().mockResolvedValue('foo'),
+}));
+
+describe("Test run", () => {
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("Test run with slauth token type", async () => {
+    const audience = "micros-service";
+    const envType = "prod";
+    const slauthGroup = "grp";
+    const tokenType = "slauth";
+    const output = await run("", tokenType, audience, envType, slauthGroup, "");
+    expect(output).toBe("invalid value defined for `asapConfigFilePath`");
+  });
+
+  test("Test run with slauth token type when envType not provided", async () => {
+    const audience = "micros-service";
+    const envType = "";
+    const slauthGroup = "grp";
+    const tokenType = "slauth";
+    const output = await run("", tokenType, audience, envType, slauthGroup, "");
+    expect(output).toBe("invalid value defined for `envType`");
+  });
+
+  test("Test run with slauth token type when slauthGroup not provided", async () => {
+    const audience = "micros-service";
+    const envType = "prod";
+    const slauthGroup = "";
+    const tokenType = "slauth";
+    const output = await run("", tokenType, audience, envType, slauthGroup, "");
+    expect(output).toBe("invalid value defined for `slauthGroup`");
+  });
+
+  test("Test run with slauth token type when audience not provided", async () => {
+    const audience = "";
+    const envType = "prod";
+    const slauthGroup = "grp";
+    const tokenType = "slauth";
+    const output = await run("", tokenType, audience, envType, slauthGroup, "");
+    expect(output).toBe("invalid value defined for `audience`");
+  });
+
+  test("Test run with asap token type", async () => {
+    const audience = "micros-service";
+    const asapConfigFilePath = "grp";
+    const tokenType = "asap";
+    const output = await run("", tokenType, audience, "", "", asapConfigFilePath);
+    expect(output).toBe("invalid value defined for `asapConfigFilePath`");
+  });
+
+  test("Test run with asap token type when asapConfigFilePath is not provided", async () => {
+    const audience = "aud";
+    const tokenType = "asap";
+    const output = await run("", tokenType, audience, "", "", "");
+    expect(output).toBe("invalid value defined for `asapConfigFilePath`");
+  });
+
+  test("Test run with asap token type when audience is not provided", async () => {
+    const asapConfigFilePath = "grp";
+    const tokenType = "asap";
+    const output = await run("", tokenType, "", "", "", asapConfigFilePath);
+    expect(output).toBe("invalid value defined for `audience`");
+  });
+
+  test("Test run with invalid token type", async () => {
+    const output = await run("", "invalid", "", "", "", "");
+    expect(output).toBe("unknown token type : invalid");
+  });
+
+});
